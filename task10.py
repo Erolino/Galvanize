7d0ba669b482a3dac15406c8f84739cd280da28b
Erolino <es3349@columbia.edu>
2018-02-17 10:30:09 -0500

    adding problem#2 to task 10

--- task10.py
+++ task10.py
@@ -1,12 +1,25 @@
-#max_lists(list1,list2)
-list1=[1,2,3]
-list2=[-1,4,2]
-maxx=[]
-for ii in range(len(list1)):
-    if list1[ii]>list2[ii]:
-        maxx.append(list1[ii])
-    else:
-        maxx.append(list2[ii])
-maxx
-    
+## Problem 1
+
+def max_lists(list1,list2):
+    maxx=[]
+    for ii in range(len(list1)):
+        if list1[ii]>list2[ii]:
+            maxx.append(list1[ii])
+        else:
+            maxx.append(list2[ii])
+    return maxx
+
+## Problem 2
+
+# e.g. input mat=[[2,3],[5,6],[10,11]]
+
+def get_diagonal(mat):
+    if len(mat)>len(mat[0]):length=len(mat[0])
+    else:length=len(mat)
+    output=[]
+    for ii in range(length):
+        output.append(mat[ii][ii])
+    return output
+
+
 
